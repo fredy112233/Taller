43ad0fe171861cc59d2d3cf3a235f002c1c3d27c
fredy112233 <85260961+fredy112233@users.noreply.github.com>
2021-06-10 12:04:12 -0500

    Update wearables.py

--- wearables.py
+++ wearables.py
@@ -1,7 +1,7 @@
 import joblib
 import math
 import pandas as pd
-rf = joblib.load("models/QDA_2.pkl")  #Cargamos el QDA
+rf = joblib.load("models/QD.pkl")  #Cargamos el QDA
 
 meanVal = joblib.load("data/meanVal.pkl")  #Cargamos los valores medios
 meanVal = pd.DataFrame(meanVal)
