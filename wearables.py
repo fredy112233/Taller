f0b044aebd191cb7a40531b50d515dbbfed1b3a6
fredy112233 <85260961+fredy112233@users.noreply.github.com>
2021-06-09 20:20:40 -0500

    Update wearables.py

--- wearables.py
+++ wearables.py
@@ -2,7 +2,7 @@ import joblib
 import math
 import pandas as pd
 
-rf = joblib.load("models/rf.pkl")  #Cargamos el rf
+rf = joblib.load("models/rf.SVM")  #Cargamos el rf
 
 meanVal = joblib.load("data/meanVal.pkl")  #Cargamos los valores medios
 meanVal = pd.DataFrame(meanVal)
