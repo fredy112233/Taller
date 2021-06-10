3547847f43b05df568b51ca1d2bc1d0741d30ad4
fredy112233 <85260961+fredy112233@users.noreply.github.com>
2021-06-10 13:27:02 -0500

    Update wearables.py

--- wearables.py
+++ wearables.py
@@ -172,9 +172,9 @@ if st.button('Predict'):
 			## Results 🔍 
 			''')
 			nStress = int(rf.predict(val))
-			if nStress <2:
+			if nStress <3:
 				st.text("Estres bajo")
-			elif nStress >=3 and  nStress<4:
+			elif nStress >=3 and  nStress=<4:
 				st.text("Nivel de estres normal")
 			else:
 				st.text("Nivel de estres alto, deberias relajarte")
